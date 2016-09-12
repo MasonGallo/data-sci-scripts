39fb6c1abaaea9c8d895ace10fdc603c37f748bc
MasonGallo <MasonGallo@users.noreply.github.com>
2016-09-12 09:51:47 -0400

    Update for 16.04

--- build_datasci.sh
+++ build_datasci.sh
@@ -6,15 +6,15 @@ git config --global user.name "MasonGallo"
 git config --global user.email "MasonGallo@users.noreply.github.com"
 
 # setup R
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
+sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" >> /etc/apt/sources.list'
 gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install r-base r-base-dev
 sudo apt-get -y install libcurl4-gnutls-dev libxml2-dev libssl-dev
-sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install openjdk-8-jdk
 sudo apt-get -y install r-cran-rgl libcairo-dev
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/i386:$JAVA_HOME/jre/lib/i386/client
 sudo R CMD javareconf
 echo -e "\nlocal({r <- getOption('repos')\nr['CRAN'] <- 'https://cran.rstudio.com'\noptions(repos=r)\n})" >> ~/.Rprofile
